e43678185a7527b96501da75703387fa277f40b6
Bob Stanley <dakotasfacts@gmail.com>
2023-11-23 23:01:48 +0000

    Added getComputerChoice Functionality
    this function uses Math.random and floor to generate a random number from 1 to 3. Then assigns and returns this numbers as either rock, paper, scissor.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    let randomValue;
+    randomValue = Math.floor(Math.random() * 3) + 1; // Generates a random integer between 1 and 3
+
+    if (randomValue == 1) {
+        return 'rock';
+    }
+
+    else if (randomValue == 2) {
+        return 'paper';
+    }
+
+    else if (randomValue == 3) {
+        return 'scissor';
+    }
+}
