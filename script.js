e3ce7dd93d5d9193a8d22bd592857ea2800cbf9f
Bob Stanley <dakotasfacts@gmail.com>
2023-11-23 23:42:29 +0000

    Still on player-round

--- script.js
+++ script.js
@@ -11,6 +11,54 @@ function getComputerChoice () {
     }
 
     else if (randomValue == 3) {
-        return 'scissor';
+        return 'scissors';
     }
+}
+
+let playerSelect = (prompt('Enter your choice: eg; rock, paper and scissors')).toLowerCase();
+
+let computerSelection = getComputerChoice();
+
+singleRound();
+
+const results = singleRound();
+
+function singleRound (playerSelect, computerSelection) {
+    let countComputer, countPlayer = 0;
+
+    if(playerSelect === computerSelection) {
+        return "Its a Draw!, Play Again", countComputer, countPlayer;
+    }
+
+    else if (playerSelect === 'rock' && computerSelection === 'scissors'){
+        countPlayer ++;
+        return "You Win! Rock beats Scissors", countComputer, countPlayer;
+    }
+
+    else if (playerSelect === 'rock' && computerSelection === 'paper'){
+        countComputer ++;
+        return "You Lose! Paper beats rock", countComputer, countPlayer;
+    }
+
+    else if (playerSelect === 'paper' && computerSelection === 'rock'){
+        countPlayer ++;
+        return "You Win! Paper beats rock", countComputer, countPlayer;
+    }
+
+    else if (playerSelect === 'paper' && computerSelection === 'scissors'){
+        countComputer ++;
+        return "You Lose! Scissors beats paper", countComputer, countPlayer;
+    }
+
+    else if (playerSelect === 'scissors' && computerSelection === 'rock'){
+        countComputer ++;
+        return "You Lose! rock beats scissors", countComputer, countPlayer;
+    }
+
+    else if (playerSelect === 'scissors' && computerSelection === 'paper'){
+        countPlayer ++;
+        return "You Win! scissors beats Paper", countComputer, countPlayer;
+    }
+
+    return countComputer, countPlayer;
 }
