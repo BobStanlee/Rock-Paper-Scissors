a5f3e24c735bcd77de4566ecd591ed5ee344f5f6
Bob Stanley <dakotasfacts@gmail.com>
2023-11-26 11:46:07 +0000

    Added the function UpdatesScore

--- script.js
+++ script.js
@@ -10,58 +10,91 @@ let playerChoiceImg = document.querySelector(".player-choice_img");
 // Selects the element with the class "computer-choice_img"
 let computerChoiceImg = document.querySelector(".computer-choice_img");
 
+let computerScore = 0;
+let playerScore = 0;
+let updatedPlayerScore = 0;
+let updatedComputerScore = 0;
+
 function playGame() {
   let playerChoice = "";
   let computerChoice = "";
+  let results = "";
 
-  playerSelectionChoices.forEach(choice => choice.addEventListener('click', () => {
-    playerChoice = choice.id;
+  playerSelectionChoices.forEach((choice) =>
+    choice.addEventListener("click", () => {
+      playerChoice = choice.id;
 
-    computerChoice = getComputerChoice();
-    
-    //Logic for displaying player choice
-    if (playerChoice === "rock") {
-      playerChoiceImg.src = "./assets/img/rock.png";
-    } else if (playerChoice === "paper") {
-      playerChoiceImg.src = "./assets/img/paper.png";
-    } else {
-      playerChoiceImg.src = "./assets/img/scissors.png";
-    }
+      computerChoice = getComputerChoice();
 
-    //Logic for displaying Computer choice
-    if (computerChoice === "rock") {
-      computerChoiceImg.src = "./assets/img/rock.png";
-    } else if (computerChoice === "paper") {
-      computerChoiceImg.src = "./assets/img/paper.png";
-    } else {
-      computerChoiceImg.src = "./assets/img/scissors.png";
-    }
+      //Logic for displaying player choice
+      if (playerChoice === "rock") {
+        playerChoiceImg.src = "./assets/img/rock.png";
+      } else if (playerChoice === "paper") {
+        playerChoiceImg.src = "./assets/img/paper.png";
+      } else {
+        playerChoiceImg.src = "./assets/img/scissors.png";
+      }
 
-    //call display results to screen function
-    displayResultsOnScreen(playerChoice, computerChoice);
+      //Logic for displaying Computer choice
+      if (computerChoice === "rock") {
+        computerChoiceImg.src = "./assets/img/rock.png";
+      } else if (computerChoice === "paper") {
+        computerChoiceImg.src = "./assets/img/paper.png";
+      } else {
+        computerChoiceImg.src = "./assets/img/scissors.png";
+      }
 
-    // 
-  }))
+      results = singleRound(playerChoice, computerChoice);
+
+      //call display results to screen function
+      displayResultsOnScreen(results);
+
+      //Update both player and computer scores
+      let updatedScores = updateScores(results, playerScore, computerScore);
+
+      //Updates the player-score element
+      let playerScoreContent = document.getElementById("player-score").textContent,
+      computerScoreContent = document.getElementById("computer-score").textContent;
+
+      console.log({playerScoreContent, computerScoreContent});
+     
+
+      // if (updatedScores.playerScore === 1) {
+      //   updatedPlayerScore++;
+      //   playerScoreContent = `Player: ${updatedPlayerScore}`
+      // }
+    })
+  );
 }
 
-// Function to play game for 5 rounds 
-function playFiveRounds () {
-  
+// Function updates both player and computer scores
+function updateScores(results, playerScore, computerScore) {
+  if (
+    results === "You Win! Scissors beats Paper" ||
+    results === "You Win! Paper beats Rock" ||
+    results === "You Win! Rock beats Scissors"
+  ) {
+    playerScore++;
+  } else if (results === "It's a Draw! Play Again") {
+    playerScore = playerScore;
+    computerScore = computerScore;
+  } else {
+    computerScore++;
+  }
+
+  return {computerScore, playerScore};
 }
 
 // function to update the number of Rounds to the user
 function updateRounds(loopnumber) {
-  let displayRound = document.querySelector(".rounds")
-  
+  let displayRound = document.querySelector(".rounds");
+
   displayRound.textContent = `Round: ${loopnumber} / 5`;
 }
 
-playGame();
-
 // Function to display result from single round game
 
-function displayResultsOnScreen(playerChoice, computerChoice) {
-  let results = singleRound(playerChoice, computerChoice);
+function displayResultsOnScreen(results) {
   displayResults.textContent = results;
 }
 
@@ -103,3 +136,5 @@ function singleRound(playerSelect, computerSelection) {
 
   return result;
 }
+
+playGame();
